ae87e72b503af4fffaeabb1f6bb3aa4631af6efa
Wole Ajewole <wolemercy@gmail.com>
2021-07-27 00:13:04 +0100

    updated ALLOWED_HOSTS with site and dev server URL

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -37,10 +37,9 @@ def get_config(setting, secrets=config):
 SECRET_KEY = get_config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['weburlshortener.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
